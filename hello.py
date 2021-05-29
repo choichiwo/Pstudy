20f1c8c7d797fc4cac9e5e1e756b00aa328d8e89
chiwon <choichiwo@naver.com>
2021-05-29 16:14:30 +0900

    수정

--- hello.py
+++ hello.py
@@ -1,5 +1,38 @@
-x = 3
-y = 5
-str = "Hello"
+x="Hello"
+y="world"
+print("Hello")
+print(x)
 
-print(str)
+# 변수형에는 정수형(3)int, 실수형(3.14)float, 문자열("Hello")str
+
+print(type(x))
+
+print(7//2) #정수나누기 몫
+print(7%2) #나머지연산자 나머지
+print(7/3)
+
+print(x+y)
+
+x=3
+print(5*"hello") #이항연사자중 *만 정수와 문자열로만 가능 
+
+print("x =", x)
+
+
+x = 13
+y = 25
+print("x =", x,",","y =", y,",", "sum =", x+y,",", "multi =" ,x*y,",", "rem =", x%y  )
+
+
+pi = 3.14159265
+
+r = 5
+
+print("원주율 =", pi)
+print("반지름 =", r)
+print("원의 둘레 =", 2*pi*r)
+print("원의 넓이 =", pi*r*r )
+
+num = input("숫자를 입력하세요")
+num = int(num)
+print(num +3)
