f2b6c3c418febe20bad43ba4133f4d2bc7580c88
chiwon <ccu12345@naver.com>
2021-06-19 14:49:26 +0900

    4교시 커밋

--- hello.py
+++ hello.py
@@ -293,4 +293,31 @@ while x != "":
     student.append({'name':x})
     x = input('이름: ')
 
-print(student)        
+print(student) 
+
+student = []
+a = 0
+b = 0
+c = 0
+d = 0
+
+x = input('이름: ')
+y = int(input('영어점수:'))
+z = int(input('수학점수:'))
+
+
+while x != "":
+    student.append({'name':x,'영어점수':y,'수학점수':z})
+    a = a+y
+    b = b+z
+
+    x = input('이름: ')
+    if x != "":
+        y = int(input('영어점수:'))
+        z = int(input('수학점수:'))
+        
+c = a / len(student)
+d = b / len(student)        
+ 
+print("영어점수총합{}, 수학점수총합{}, 영어평균{:.1f}, 수학평균{:.1f}".format(a,b,c,d))
+
