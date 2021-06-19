160f1be034bd56a40a25edd48d96246b431cebf3
chiwon <ccu12345@naver.com>
2021-06-19 12:01:15 +0900

    2교시 커밋

--- assignment.py
+++ assignment.py
@@ -1,25 +1,9 @@
-#break 해당 조건이 충족되면 밖으로 빠져나옴, 가장 안쪽의 반복문 탈출
+student = []
 
-for i in range(10):
-    if i > 7:
-        break
-    print(i)
-i = 0
-while i < 10:
-    print(i)
-    if i >7:
-        break
-    i = i+1
+x = input('이름: ')
 
-#continue 해당 조건이 충족 되면 다시 위에 조건을 실시
+while x != "":
+    student.append({'name':x})
+    x = input('이름: ')
 
-for a in range(10):
-    if a > 7:
-        continue
-    print(a)
-
-while a < 10:
-    print(a)
-    if a >7:
-        continue #잘못 사용시 무한루프에 걸림
-    a = a+1
+print(student)    
