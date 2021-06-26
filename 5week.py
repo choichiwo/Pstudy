f1f6c7209adc5c2f2040b93fa915816c5575c22b
chiwon <ccu12345@naver.com>
2021-06-26 15:01:33 +0900

    6교시 커밋

--- 5week.py
+++ 5week.py
@@ -62,4 +62,36 @@ def pluss(n):
         return 0
     else:
         return n+pluss(n-1)
-print(pluss(100))        
+print(pluss(100))      
+
+# return 1 값을 반호나 return 값
+#        2 함수종료 return
+
+# 매개변수:  <=매개변수갯수
+# def f1(a=3, b=5, c=10):
+# f1(2,3)   = f1(2,3,10)
+# f1() = f1(3,5,10)
+# print(fi)
+
+# 가변 매개변수: >=매개변수갯수
+# def f1(a, *n):
+#     sum = 0
+#     for i in n:
+#         sum = sum + i
+#         print(sum)
+
+# f1(1,2,3)        
+# f1(1,3)
+# f1(1)
+# f1(1,23,4,2,6,8,7,9,12)
+
+f = open("c:/document/menu.txt","w") #.w = for write(없으면 생성하고 열기, 있으면 기존내용 삭제하구 열기), .a = for append(없으면 생성하고 열기, 기존내용 유지 뒤에 새 내용 추가), .r = for read()
+
+file = open("basic.txt","w")
+file.write("Hello Python Programming...")
+file.close()
+
+
+file = open("basic.txt","r")
+line=file.read()
+file.close()
--- hello.py
+++ hello.py
@@ -1,20 +1,12 @@
-# return 1 값을 반호나 return 값
-#        2 함수종료 return
+#file = open("d:/menu.txt","w")
+with open("d:/menu.txt","w") as file: #close를 안써두 됨
+file.write("모카 3500\n") #\n 줄바꿈
+file.write("라떄 3000\n")
+file.write("카푸치노 3700\n")
+#file.close()
 
-# 매개변수:  <=매개변수갯수
-# def f1(a=3, b=5, c=10):
-# f1(2,3)   = f1(2,3,10)
-# f1() = f1(3,5,10)
-# print(fi)
 
-# 가변 매개변수: >=매개변수갯수
-# def f1(a, *n):
-#     sum = 0
-#     for i in n:
-#         sum = sum + i
-#         print(sum)
-
-# f1(1,2,3)        
-# f1(1,3)
-# f1(1)
-# f1(1,23,4,2,6,8,7,9,12)
+file = open("d:/menu.txt","r")
+line=file.read()
+print(line)
+file.close()
