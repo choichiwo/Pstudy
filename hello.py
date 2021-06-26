df263cf17d715fe30fce8a1983657f559dc4b6a3
chiwon <ccu12345@naver.com>
2021-06-26 16:45:11 +0900

    6교시 커밋

--- hello.py
+++ hello.py
@@ -1,12 +1,2 @@
-try:
-    ......
-except ValueError:
-    print("값 오류")     
-except IndexError:
-    print("OutOfBoundIndexError")   
-except Exception as e: 
-    print("기타 일반오류")      
-else:
-    print("No error")    
-finally:
-    print("프로그램 종료")  
+
+
