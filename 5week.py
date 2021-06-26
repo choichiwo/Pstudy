b2e0ca6cbb5fd8bd237a6f0732f27abdb25cf04b
chiwon <ccu12345@naver.com>
2021-06-26 14:02:40 +0900

    4교시 커밋

--- 5week.py
+++ 5week.py
@@ -47,3 +47,19 @@ def factor(n):
         return n*factor(n-1)    
 
 print("4!:", factor(4)) 
+
+def plus(n):
+    output = 0
+    for i in range(1, n+1):
+        output = output+i
+    return output
+
+x = plus(100)
+print(x)        
+
+def pluss(n):
+    if n== 0:
+        return 0
+    else:
+        return n+pluss(n-1)
+print(pluss(100))        
--- hello.py
+++ hello.py
@@ -1,16 +1,20 @@
-def plus(n):
-    output = 0
-    for i in range(1, n+1):
-        output = output+i
-    return output
+# return 1 값을 반호나 return 값
+#        2 함수종료 return
 
-x = plus(100)
-print(x)        
+# 매개변수:  <=매개변수갯수
+# def f1(a=3, b=5, c=10):
+# f1(2,3)   = f1(2,3,10)
+# f1() = f1(3,5,10)
+# print(fi)
 
-def pluss(n):
-    if n== 0:
-        return 0
-    else:
-        return n+pluss(n-1)
-print(pluss(100))        
+# 가변 매개변수: >=매개변수갯수
+# def f1(a, *n):
+#     sum = 0
+#     for i in n:
+#         sum = sum + i
+#         print(sum)
 
+# f1(1,2,3)        
+# f1(1,3)
+# f1(1)
+# f1(1,23,4,2,6,8,7,9,12)
