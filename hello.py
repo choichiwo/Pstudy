56a3e0447f8b8d86123e79db2d6b9062a59be582
chiwon <ccu12345@naver.com>
2021-06-26 11:41:03 +0900

    2교시 커밋

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
+def plus(n):
+    output = 0
+    for i in range(1, n+1):
+        output = output+1
+    return output
 
+x = plus(10)
+print(x)        
+
+def pluss(n):
+    if n== 1:
+        return 1
+    else:
+        return n+pluss(n-1)
+print(pluss(10))        
 
